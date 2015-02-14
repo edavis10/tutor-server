5483cf7167383e171b94cde1f50eb642d82a2d3b
Kevin Burleigh <klb@kindling>
2015-02-14 17:37:00 -0500

    render --> respond_with; inserted module namespace

--- app/controllers/api/v1/kevins_controller.rb
+++ app/controllers/api/v1/kevins_controller.rb
@@ -1,15 +1,17 @@
-class Api::V1::KevinsController < Api::V1::ApiController
+module Api::V1
+  class KevinsController < ApiController
 
-  def index
-    json = {field1: 0, field2: [1,2,3]}
-    render json: Api::V1::KevinFixedJsonRepresenter.new(Hashie::Mash.new(json)).to_json, status: :ok
+    def index
+      json = {field1: 0, field2: [1,2,3]}
+      render json: KevinFixedJsonRepresenter.new(Hashie::Mash.new(json)).to_json, status: :ok
+    end
+
+    def convert
+      stig = Hashie::Mash.new(field1: nil, field2: nil)
+      consume!(stig, represent_with: KevinFixedJsonRepresenter)
+      stig.field1 = stig.field1 + 1
+      respond_with stig, represent_with: KevinFixedJsonRepresenter
+    end
+
   end
-
-  def convert
-    stig = Hashie::Mash.new(field1: nil, field2: nil)
-    consume!(stig, represent_with: Api::V1::KevinFixedJsonRepresenter)
-    stig.field1 = stig.field1 + 1
-    render json: Api::V1::KevinFixedJsonRepresenter.new(stig).to_json, status: :ok
-  end
-
 end
