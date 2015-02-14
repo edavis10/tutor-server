c9a0afbc0c1a24ffcc699545638f5bd3c1cd7eee
Kevin Burleigh <klb@kindling>
2015-02-14 17:10:29 -0500

    added KevinFixedJsonRepresenter

--- app/controllers/api/v1/kevins_controller.rb
+++ app/controllers/api/v1/kevins_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::KevinsController < Api::V1::ApiController
 
   def index
-    render json: {field1: 0, field2: [1,2,3]}, status: :ok
+    json = {field1: 0, field2: [1,2,3]}
+    render json: Api::V1::KevinFixedJsonRepresenter.new(Hashie::Mash.new(json)).to_json, status: :ok
   end
 
 end
---
+++ app/representers/api/v1/kevin_fixed_json_representer.rb
@@ -0,0 +1,21 @@
+module Api::V1
+  class KevinFixedJsonRepresenter < Roar::Decorator
+
+    include Roar::Representer::JSON
+
+    property :field1,
+             type: Integer,
+             readable: true,
+             writeable: false,
+             schema_info: {
+               description: "field1"
+             }
+
+    collection :field2,
+               readable: true,
+               writeable: false,
+               schema_info: {
+                 description: "field2"
+               }
+  end
+end
