1f046f6291d06355fcce0b7966e2dfffa9039419
Joe Sak <joe@joesak.com>
2015-02-24 17:50:08 -0500

    Assign book and child book #path on Import::Book routine

--- app/routines/import/book.rb
+++ app/routines/import/book.rb
@@ -14,15 +14,17 @@ module Import
     # Recursively imports items in a CNX collection into the given book
     def import_collection(parent_book, hash, options = {})
       book = ::Book.create(parent_book: parent_book,
-                           title: hash['title'] || '')
+                           title: hash['title'] || '',
+                           path: construct_path(parent_book, options[:path_index]))
 
       parent_book.child_books << book unless parent_book.nil?
 
-      hash['contents'].each do |item|
+      hash['contents'].each_with_index do |item, i|
         if item['id'] == 'subcol'
+          options[:path_index] = i
           import_collection(book, item, options)
         else
-          run(:page_import, item['id'], book, 
+          run(:page_import, item['id'], book,
                             options.merge(title: item['title']))
         end
       end
@@ -41,6 +43,16 @@ module Import
       outputs[:book].save
 
       transfer_errors_from outputs[:book], type: :verbatim
+    end
+
+    def construct_path(parent_book, path_index)
+      if parent_book.respond_to?(:path) && !parent_book.path.blank?
+        path = "#{parent_book.path}"
+        path += ".#{path_index + 1}" if path_index
+        path
+      else
+        "#{path_index}"
+      end
     end
 
   end
